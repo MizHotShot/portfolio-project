f2e277a9240eaeb680829fb768799dd016d46926
Ariel <noreply>
2023-10-11 13:37:19 -0400

    made ul in nav a grid, made links have no style and white

--- nav button.js
+++ nav button.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementById('toggleButton');
 const navbarContainer = document.getElementById('navbar-container');
+const navbarElements = document.getElementById('navbar');
 
 toggleButton.addEventListener('click', () => {
     navbarContainer.classList.toggle('open'); // Toggle the 'open' class
+    navbarElements.classList.toggle('open');
 });
