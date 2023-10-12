55516bd315589d32bc8296b353f18d23eff7b2f2
Ariel <noreply>
2023-10-12 16:50:32 -0400

    made entire main page resize to accomodate navbar hover and toggle

--- nav button.js
+++ nav button.js
@@ -2,9 +2,38 @@ const toggleButton = document.getElementById('toggleButton');
 const navbarContainer = document.getElementById('navbar-container');
 const navbarElements = document.getElementById('navbar');
 const plusSign = document.getElementById('plus');
+const main = document.querySelector('main'); // Get the <main> element
+
+// Store the default margin-top value of <main>
+const mainDefaultMarginTop = parseFloat(getComputedStyle(main).marginTop);
+
+// Function to move the main element
+function moveMain() {
+    main.style.marginTop = `${navbarElements.offsetHeight}px`;
+}
+
+// Function to reset the main element's margin
+function resetMainMargin() {
+    main.style.marginTop = `${mainDefaultMarginTop}px`;
+}
+
+toggleButton.addEventListener('mouseenter', () => {
+    moveMain();
+});
+
+toggleButton.addEventListener('mouseleave', () => {
+    resetMainMargin();
+});
 
 toggleButton.addEventListener('click', () => {
-    navbarContainer.classList.toggle('open'); // Toggle the 'open' class
+    navbarContainer.classList.toggle('open');
     navbarElements.classList.toggle('open');
     plusSign.classList.toggle('open');
+
+    // After toggling, check if the navbar is open and adjust the main margin
+    if (navbarContainer.classList.contains('open')) {
+        moveMain();
+    } else {
+        resetMainMargin();
+    }
 });
