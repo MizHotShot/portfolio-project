ce2993d36f94a7c7864e25cb6d1be4871a15032b
Ariel <noreply>
2023-10-12 15:46:28 -0400

    finalized animations and toggles for nav

--- nav button.js
+++ nav button.js
@@ -1,8 +1,10 @@
 const toggleButton = document.getElementById('toggleButton');
 const navbarContainer = document.getElementById('navbar-container');
 const navbarElements = document.getElementById('navbar');
+const plusSign = document.getElementById('plus');
 
 toggleButton.addEventListener('click', () => {
     navbarContainer.classList.toggle('open'); // Toggle the 'open' class
     navbarElements.classList.toggle('open');
+    plusSign.classList.toggle('open');
 });
