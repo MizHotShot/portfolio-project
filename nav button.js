b42a975bfc44e1f60e9bddea1728efa1a73ed45a
Ariel <noreply>
2023-10-16 16:23:06 -0400

    navbar interactions perfected

--- nav button.js
+++ nav button.js
@@ -1,39 +1,70 @@
 const toggleButton = document.getElementById('toggleButton');
-const navbarContainer = document.getElementById('navbar-container');
-const navbarElements = document.getElementById('navbar');
 const plusSign = document.getElementById('plus');
 const main = document.querySelector('main'); // Get the <main> element
+const navbarContainer = document.getElementById('navbar-container');
+const navbarElements = document.getElementById('navbar');
 
-// Store the default margin-top value of <main>
-const mainDefaultMarginTop = parseFloat(getComputedStyle(main).marginTop);
+// Define variables for different margin and height values
+const closedMargin = 48;
+const openMargin = 144;
+const hoverMarginIncrement = 20;
+const hoverNavbarHeightIncrement = 10;
 
-// Function to move the main element
-function moveMain() {
-    main.style.marginTop = `${navbarElements.offsetHeight}px`;
+// Function to set the main element's margin
+function setMainMargin(marginValue) {
+    main.style.marginTop = `${marginValue}px`;
+}
+
+// Function to set the navbarContainer's height
+function setNavbarContainerHeight(heightValue) {
+    navbarContainer.style.height = `${heightValue}px`;
+}
+
+// Function to handle button hover
+function moveMainOnButtonHover() {
+    if (navbarContainer.classList.contains('open')) {
+        setMainMargin(openMargin);
+        setNavbarContainerHeight(openMargin + hoverNavbarHeightIncrement);
+    } else {
+        setMainMargin(closedMargin);
+        setNavbarContainerHeight(closedMargin + hoverNavbarHeightIncrement);
+    }
 }
 
 // Function to reset the main element's margin
 function resetMainMargin() {
-    main.style.marginTop = `${mainDefaultMarginTop}px`;
+    if (navbarContainer.classList.contains('open')) {
+        setMainMargin(openMargin);
+        setNavbarContainerHeight(openMargin);
+    } else {
+        setMainMargin(closedMargin);
+        setNavbarContainerHeight(closedMargin);
+    }
 }
 
-toggleButton.addEventListener('mouseenter', () => {
-    moveMain();
-});
-
-toggleButton.addEventListener('mouseleave', () => {
-    resetMainMargin();
-});
+toggleButton.addEventListener('mouseenter', moveMainOnButtonHover);
+toggleButton.addEventListener('mouseleave', resetMainMargin);
 
 toggleButton.addEventListener('click', () => {
     navbarContainer.classList.toggle('open');
     navbarElements.classList.toggle('open');
     plusSign.classList.toggle('open');
+    resetMainMargin(); // Ensure the margin is reset when clicking the button
+});
 
-    // After toggling, check if the navbar is open and adjust the main margin
+// Add an event listener to handle hover over the navbar
+navbarContainer.addEventListener('mouseenter', () => {
     if (navbarContainer.classList.contains('open')) {
-        moveMain();
+        setNavbarContainerHeight(openMargin + hoverNavbarHeightIncrement);
     } else {
-        resetMainMargin();
+        setNavbarContainerHeight(closedMargin + hoverNavbarHeightIncrement);
+    }
+});
+
+navbarContainer.addEventListener('mouseleave', () => {
+    if (navbarContainer.classList.contains('open')) {
+        setNavbarContainerHeight(openMargin);
+    } else {
+        setNavbarContainerHeight(closedMargin);
     }
 });
